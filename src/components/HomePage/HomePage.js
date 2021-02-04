85592ce2467c4553283dd6f3fbb3452bde078254
abhijeetAbnave <abhijeetabnave1@gmail.com>
2021-02-04 12:19:03 +0530

    Final touchups on assignment

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import logo from "../../assets/images/logo.svg";
+import { Button, Col, Row } from "react-bootstrap";
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -8,72 +9,94 @@ class HomePage extends React.Component {
     this.canvasArea = React.createRef();
   }
   state = {
-    cx: [],                 //square co-ordinate x
-    cy: [],                 //square co-ordinate y 
-    selectedIndex: null,     //selected index of square
+    cx: [], //square co-ordinate x
+    cy: [], //square co-ordinate y
+    selectedIndex: null, //selected index of square
     isKeyboardOn: true,
-  }
-  stage = null;             //DOM element
-  ctx = null;               //getContext element of canvas
+  };
+  stage = null; //DOM element
+  ctx = null; //getContext element of canvas
   canvasWidth = 0;
   canvasHeight = 0;
-  constantBoundryGap = 40;  //Perimeter gap 
-  sideOfSquare = 40;        //side of square
-
+  constantBoundryGap = 40; //Perimeter gap
+  sideOfSquare = 40; //side of square
 
   componentDidMount() {
     this.stage = this.canvasArea.current;
-    this.ctx = this.stage.getContext('2d'); // Canvas 2d rendering context
+    this.ctx = this.stage.getContext("2d"); // Canvas 2d rendering context
     this.canvasWidth = this.stage.width;
     this.canvasHeight = this.stage.height;
-    this.state.cx[0] = (this.randomNumber(0, Math.floor(this.canvasWidth - this.constantBoundryGap))); // Setting initial values
-    this.state.cy[0] = (this.randomNumber(0, Math.floor(this.canvasHeight - this.constantBoundryGap)));
+    this.state.cx[0] = this.randomNumber(
+      0,
+      Math.floor(this.canvasWidth - this.constantBoundryGap)
+    ); // Setting initial values
+    this.state.cy[0] = this.randomNumber(
+      0,
+      Math.floor(this.canvasHeight - this.constantBoundryGap)
+    );
     this.drawSquare(this.state.cx[0], this.state.cy[0], this.ctx, 1);
-    document.addEventListener("keydown", event => this.keyboardEventFunction(event))
+    document.addEventListener("keydown", (event) =>
+      this.keyboardEventFunction(event)
+    );
   }
 
   keyboardEventFunction = (event) => {
     if (this.state.selectedIndex !== null && this.state.isKeyboardOn)
       this.handleKeyDown(event);
-  }
+  };
 
   drawSquare = (x, y, ctx, opacity) => {
     // ctx.fillStyle = '#FF8010'; // Fill color of rectangle drawn
     ctx.globalCompositeOperation = "source-over";
-    ctx.fillStyle = `rgba(${231 - (1 / opacity * 7)}, ${66 + (1 / opacity * 6)}, ${41 + (1 / opacity * 3)}, ${isFinite(opacity) ? (opacity === 1 ? 0.9 : 1 / (1 - opacity)) : 0.9})`; // Fill color of rectangle drawn
+    ctx.fillStyle = `rgba(${231 - (1 / opacity) * 7}, ${
+      66 + (1 / opacity) * 6
+    }, ${41 + (1 / opacity) * 3}, ${
+      isFinite(opacity) ? (opacity === 1 ? 0.9 : 1 / (1 - opacity)) : 0.9
+    })`; // Fill color of rectangle drawn
     ctx.fillRect(x, y, this.sideOfSquare, this.sideOfSquare); //This will draw a rectangle of 20x20
-  }
+  };
 
   selectedSquare = (x, y, ctx, opacity) => {
-
     ctx.globalCompositeOperation = "source-over";
-    ctx.fillStyle = `rgba(0, 0, 0, ${isFinite(opacity) ? (opacity === 1 ? 0.9 : 1 / (1 - opacity)) : 0.9})`; // Fill color of rectangle drawn
+    ctx.fillStyle = `rgba(0, 0, 0, ${
+      isFinite(opacity) ? (opacity === 1 ? 0.9 : 1 / (1 - opacity)) : 0.9
+    })`; // Fill color of rectangle drawn
     ctx.fillRect(x, y, this.sideOfSquare, this.sideOfSquare); //This will draw a rectangle of 20x20
-  }
+  };
 
   randomNumber = (min, max) => {
-    return Math.floor(Math.random() * (max - min) + min)
-  }
+    return Math.floor(Math.random() * (max - min) + min);
+  };
 
   onAdd = () => {
     let index = this.state.cx.length;
-    this.state.cx[index] = (this.randomNumber(0, Math.floor(this.canvasWidth - this.constantBoundryGap)));
-    this.state.cy[index] = (this.randomNumber(0, Math.floor(this.canvasHeight - this.constantBoundryGap)));
-    this.drawSquareWithSelected()
+    this.state.cx[index] = this.randomNumber(
+      0,
+      Math.floor(this.canvasWidth - this.constantBoundryGap)
+    );
+    this.state.cy[index] = this.randomNumber(
+      0,
+      Math.floor(this.canvasHeight - this.constantBoundryGap)
+    );
+    this.drawSquareWithSelected();
     // this.drawSquare(this.state.cx[index], this.state.cy[index], this.ctx, 1 / index);
-  }
+  };
 
   onDelete = () => {
     this.deleteSquare(this.state.selectedIndex);
-  }
+  };
 
   canvasClick = (e) => {
-    let rect = this.stage.getBoundingClientRect()
-    console.log("canvas click ", e.clientX - rect.left, " y ", e.clientY - rect.top);
+    let rect = this.stage.getBoundingClientRect();
+    console.log(
+      "canvas click ",
+      e.clientX - rect.left,
+      " y ",
+      e.clientY - rect.top
+    );
     console.log("canvas click ", e);
     console.log("canvas click ", this.state.cx[0], " y ", this.state.cy[0]);
-
-  }
+  };
 
   relMouseCoords(event) {
     var totalOffsetX = 0;
@@ -85,8 +108,7 @@ class HomePage extends React.Component {
     do {
       totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
       totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
-    }
-    while (currentElement = currentElement.offsetParent)
+    } while ((currentElement = currentElement.offsetParent));
 
     canvasX = event.pageX - totalOffsetX;
     canvasY = event.pageY - totalOffsetY;
@@ -97,28 +119,50 @@ class HomePage extends React.Component {
   showSelectedSquare = (canvasX, canvasY) => {
     let isSelected = false;
 
-    this.ctx.clearRect(0, 0, 800, 400);     
+    this.ctx.clearRect(0, 0, 800, 400);
     for (let index = 0; index < this.state.cx.length; index++) {
-      if (this.isPointInsideOfSquare(canvasX, canvasY, this.state.cx[index], this.state.cy[index]) && !isSelected) {
+      if (
+        this.isPointInsideOfSquare(
+          canvasX,
+          canvasY,
+          this.state.cx[index],
+          this.state.cy[index]
+        ) &&
+        !isSelected
+      ) {
         isSelected = true;
-        console.log("selected ", index)
+        console.log("selected ", index);
         this.setState({ selectedIndex: index });
-        this.selectedSquare(this.state.cx[index], this.state.cy[index], this.ctx, 1 / index);
+        this.selectedSquare(
+          this.state.cx[index],
+          this.state.cy[index],
+          this.ctx,
+          1 / index
+        );
       } else {
-        this.drawSquare(this.state.cx[index], this.state.cy[index], this.ctx, 1 / index);
+        this.drawSquare(
+          this.state.cx[index],
+          this.state.cy[index],
+          this.ctx,
+          1 / index
+        );
       }
     }
     if (!isSelected) {
-      this.setState({ selectedIndex: null })
+      this.setState({ selectedIndex: null });
     }
-  }
+  };
 
   isPointInsideOfSquare = (canvasX, canvasY, squareX, squareY) => {
-    if (canvasX >= squareX && canvasX <= (squareX + this.sideOfSquare) && canvasY >= squareY && canvasY <= (squareY + this.sideOfSquare))
+    if (
+      canvasX >= squareX &&
+      canvasX <= squareX + this.sideOfSquare &&
+      canvasY >= squareY &&
+      canvasY <= squareY + this.sideOfSquare
+    )
       return true;
-    else
-      return false;
-  }
+    else return false;
+  };
 
   deleteSquare = (index) => {
     if (index != null) {
@@ -129,37 +173,44 @@ class HomePage extends React.Component {
       this.ctx.clearRect(0, 0, 800, 400);
       this.drawSquareWithSelected();
     }
-  }
+  };
 
   drawSquareWithSelected = () => {
     for (let index = 0; index < this.state.cx.length; index++) {
       if (this.state.selectedIndex == index) {
-        this.selectedSquare(this.state.cx[index], this.state.cy[index], this.ctx, 1 / index);
+        this.selectedSquare(
+          this.state.cx[index],
+          this.state.cy[index],
+          this.ctx,
+          1 / index
+        );
       } else {
         // this.ctx.clearRect(0,0, 800, 400);
-        this.drawSquare(this.state.cx[index], this.state.cy[index], this.ctx, 1 / index);
+        this.drawSquare(
+          this.state.cx[index],
+          this.state.cy[index],
+          this.ctx,
+          1 / index
+        );
       }
     }
-  }
+  };
 
   handleKeyDown = (event) => {
-    console.log("handleKeyDown")
+    console.log("handleKeyDown");
     let keyPr = event.keyCode; //Key code of key pressed
     if (keyPr === 46) {
       this.onDelete();
       return;
     }
     if (keyPr === 68 && this.state.cx[this.state.selectedIndex] <= 770) {
-      this.state.cx[this.state.selectedIndex] += 20; //D key add 20 
-    }
-    else if (keyPr === 65 && this.state.cx[this.state.selectedIndex] > 10) {
-      this.state.cx[this.state.selectedIndex] -= 20; //A key subtract 20 
-    }
-    else if (keyPr === 87 && this.state.cy[this.state.selectedIndex] > 10) {
-      this.state.cy[this.state.selectedIndex] -= 20; //W key subtract 20 
-    }
-    else if (keyPr === 83 && this.state.cy[this.state.selectedIndex] <= 380) {
-      this.state.cy[this.state.selectedIndex] += 20; //S key add 20 
+      this.state.cx[this.state.selectedIndex] += 20; //D key add 20
+    } else if (keyPr === 65 && this.state.cx[this.state.selectedIndex] > 10) {
+      this.state.cx[this.state.selectedIndex] -= 20; //A key subtract 20
+    } else if (keyPr === 87 && this.state.cy[this.state.selectedIndex] > 10) {
+      this.state.cy[this.state.selectedIndex] -= 20; //W key subtract 20
+    } else if (keyPr === 83 && this.state.cy[this.state.selectedIndex] <= 380) {
+      this.state.cy[this.state.selectedIndex] += 20; //S key add 20
     }
 
     // clearing anything drawn on canvas
@@ -169,28 +220,112 @@ class HomePage extends React.Component {
     this.drawSquareWithSelected();
   };
 
-  toggleKeyboard = async() => {
+  toggleKeyboard = async () => {
     await this.setState({ isKeyboardOn: !this.state.isKeyboardOn });
     if (this.state.isKeyboardOn) {
-      await document.addEventListener("keydown",event => this.keyboardEventFunction(event))
+      await document.addEventListener("keydown", (event) =>
+        this.keyboardEventFunction(event)
+      );
     } else {
-      await document.removeEventListener("keydown", event => this.keyboardEventFunction(event), true)
+      await document.removeEventListener(
+        "keydown",
+        (event) => this.keyboardEventFunction(event),
+        true
+      );
     }
-  }
+  };
 
   render() {
     return (
       <Fragment>
         <div id="magic-rectangle">
-          <canvas onClick={(e) => this.relMouseCoords(e)} width="800" height="400" id="svs" ref={this.canvasArea} className="canvas" ></canvas>
-          <div className="col-12">
-            <button onClick={() => this.onAdd()} className="button button-add">Add</button>
-            <button onClick={() => this.onDelete()} className="button button-delete">Delete</button>
-          </div>
-          <div className="col-12">
-            <button onClick={() => this.toggleKeyboard()} className="button button-toggle">Keyboard Control {this.state.isKeyboardOn ? "On" : "Off"}</button>
-          </div>
+          <canvas
+            onClick={(e) => this.relMouseCoords(e)}
+            width="800"
+            height="400"
+            id="svs"
+            ref={this.canvasArea}
+            className="canvas"
+          ></canvas>
+          {/* <div className="row">
+            <div className="col-12"> */}
+          <Row className="info">
+            <Col md={1}></Col>
+            <Col md={4}>
+              <Row>
+                <Col md={12}>
+                  <button
+                    onClick={() => this.onAdd()}
+                    className="button button-add"
+                  >
+                    Add
+                  </button>
+                  <button
+                    onClick={() => this.onDelete()}
+                    className="button button-delete"
+                  >
+                    Delete
+                  </button>
+                </Col>
+                <Col md={12}>
+                  <button
+                    onClick={() => this.toggleKeyboard()}
+                    className="button button-toggle"
+                  >
+                    Keyboard Control {this.state.isKeyboardOn ? "On" : "Off"}
+                  </button>
+                </Col>
+              </Row>
+            </Col>
+            <Col md={5}>
+              <Row>
+                <Col md={12}>
+                  <div className="button-lable">
+                    Use Keyboard to move rectangle
+                  </div>
+                  <br />
+                  <Row className="">
+                    <Col md={6}>
+                      <Row>
+                        <Col md={6}>
+                          <button className="button-arrows-main-vertical button-arrows">
+                            &uarr;
+                          </button>{" "}
+                          <p className="button-lable">Press W</p>
+                        </Col>
+                        <Col md={6}>
+                          <button className="button-arrows-main-vertical button-arrows">
+                            &darr;
+                          </button>{" "}
+                          <p className="button-lable">Press S</p>
+                        </Col>
+                      </Row>
+                    </Col>
+                    <Col md={6}>
+                      <Row>
+                        <Col md={6}>
+                          <button className="button-arrows-main-horizontal button-arrows">
+                            &larr;
+                          </button>{" "}
+                          <p className="button-lable">Press A</p>
+                        </Col>
+                        <Col md={6}>
+                          <button className="button-arrows-main-horizontal button-arrows">
+                            &rarr;
+                          </button>{" "}
+                          <p className="button-lable">Press D</p>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Col>
+            <Col md={1}></Col>
+          </Row>
         </div>
+        {/* </div>
+        </div> */}
       </Fragment>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import "bootstrap/dist/js/bootstrap.min.js"
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
